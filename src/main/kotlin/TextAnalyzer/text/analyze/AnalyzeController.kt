0d29518812c434653568cf9637f1534cdfeb52e3
sankar <sankar06.ms@gmail.com>
2022-11-28 15:51:17 +0530

    Add functionality for AnalyzeContoller
    The functionality for the route : ".../text/analyze" was added and
    tested the working of the endpoint.

--- src/main/kotlin/TextAnalyzer/text/analyze/AnalyzeController.kt
+++ src/main/kotlin/TextAnalyzer/text/analyze/AnalyzeController.kt
@@ -18,8 +18,42 @@ class AnalyzeController {
     @Post()
     fun textanalyzer(text:JsonObject) : String? {
         var content: String = text.get("content").coerceStringValue()
-        
-        return "Endpoint working!"
+        val word_count = getWordCount(content)
+        val character_count_with_spaces = getCharacterCountWithSpaces(content)
+        val character_count_without_spaces = getCharacterCountWithoutSpaces(content)
+        val line_count = getLineCount(content)
+        val unique_words = getUniqueWords(content)
+
+        val res = Result(word_count,character_count_with_spaces,character_count_without_spaces,line_count,unique_words)
+        return Gson().toJson(res)
     }
 
+    fun getUniqueWords(text: String):Int{
+        val list = text.replace("\n","").split(" ")
+        var count = 0
+        var uniqueWords : HashSet<String> = HashSet<String>(list)
+        uniqueWords.forEach { it ->
+            if (it == " ") {
+                count++
+            }
+        }
+        return uniqueWords.size - count
+    }
+    fun getLineCount(text: String):Int{
+        var line = 1
+        for(i in text.split("")){
+            if(i == "\n"){ line++ }
+        }
+        return line
+    }
+    fun getCharacterCountWithoutSpaces(text: String):Int {
+        var temp = text.replace("\n","")
+        return temp.replace(" ","").length
+    }
+    fun getCharacterCountWithSpaces(text: String):Int {
+        return text.replace("\n","").length
+    }
+    fun getWordCount(text: String): Int {
+        return text.split(" ").size
+    }
 }
